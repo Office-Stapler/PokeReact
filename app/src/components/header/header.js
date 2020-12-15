fe56e23cdc77253e934707d272cdd8a1ba059cdf
Office-Stapler <raymond.lam@outlook.com.au>
2020-12-15 16:11:17 +1100

    Improved header design

--- app/src/components/header/header.js
+++ app/src/components/header/header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import pokedex from '../../data/pokedex.png';
+import './header.css';
+
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        
+        return (
+            <div className="header">
+                <h1>Pokedex</h1>
+                <img src={pokedex} width="57.25" height="67.75"></img>
+            </div>
+        )
     }
 }
