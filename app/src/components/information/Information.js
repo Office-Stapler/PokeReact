3ab60b94f447cb9d6e8f819a93729353ad81ffb3
Office-Stapler <raymond.lam@outlook.com.au>
2020-12-15 18:43:55 +1100

    Changed stats to iterable

--- app/src/components/information/Information.js
+++ app/src/components/information/Information.js
@@ -7,12 +7,20 @@ export default class Information extends React.Component {
     constructor(props) {
         super(props);
         this.pokeAPI = 'https://pokeapi.co/api/v2/';
+        this.colours = {
+            "hp": "green",
+            'attack': "darkred",
+            "defense": "darkblue",
+            "special-attack": "red",
+            "special-defense": "blue",
+            "speed": "yellow"
+        }
     }
 
     render() {
+        
         this.info = this.props.pokeInfo;
         if (this.props.pokeInfo != null) {
-            this.stats = this.info.stats;
             return (
                 <div className="info">
                     <div className="NameAndImage">
@@ -26,151 +34,7 @@ export default class Information extends React.Component {
                             <tr>
                                 <th>Base Stats</th>
                             </tr>
-                            <tr>
-                                <td>
-                                    <div 
-                                    style={{
-                                        float: "left"
-                                    }}
-
-                                    >HP</div>
-                                    <div style={{    
-                                        float: "right"
-                                    }}>{this.stats[0]['base_stat']}</div>
-                                </td>
-                                <td className="statLabel">
-                                    <div className="statbar" style= {{
-                                            width: this.stats[0]['base_stat'] / 255 * 100,
-                                            backgroundColor: "green",
-                                            border: "1px solid black",
-                                            height: "20px"
-                                        }
-                                    }
-                                    >
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div 
-                                    style={{
-                                        float: "left"
-                                    }}
-
-                                    >Attack</div>
-                                    <div style={{    
-                                        float: "right"
-                                    }}>{this.stats[2]['base_stat']}</div>
-                                </td>
-                                <td className="statLabel">
-                                    <div className="statbar" style= {{
-                                            width: this.stats[2]['base_stat'] / 255 * 100,
-                                            backgroundColor: "darkred",
-                                            border: "1px solid black",
-                                            height: "20px"
-                                        }
-                                    }
-                                    >
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div 
-                                    style={{
-                                        float: "left"
-                                    }}
-
-                                    >Defense</div>
-                                    <div style={{    
-                                        float: "right"
-                                    }}>{this.stats[1]['base_stat']}</div>
-                                </td>
-                                <td className="statLabel">
-                                    <div className="statbar" style= {{
-                                            width: this.stats[2]['base_stat'] / 255 * 100,
-                                            backgroundColor: "darkblue",
-                                            border: "1px solid black",
-                                            height: "20px"
-                                        }
-                                    }
-                                    >
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div 
-                                    style={{
-                                        float: "left"
-                                    }}
-
-                                    >Sp. Attack</div>
-                                    <div style={{    
-                                        float: "right"
-                                    }}>{this.stats[3]['base_stat']}</div>
-                                </td>
-                                <td className="statLabel">
-                                    <div className="statbar" style= {{
-                                            width: this.stats[3]['base_stat'] / 255 * 100,
-                                            backgroundColor: "red",
-                                            border: "1px solid black",
-                                            height: "20px"
-                                        }
-                                    }
-                                    >
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div 
-                                    style={{
-                                        float: "left"
-                                    }}
-
-                                    >Sp. Defense</div>
-                                    <div style={{    
-                                        float: "right"
-                                    }}>{this.stats[4]['base_stat']}</div>
-                                </td>
-                                <td className="statLabel">
-                                    <div className="statbar" style= {{
-                                            width: this.stats[4]['base_stat'] / 255 * 100,
-                                            backgroundColor: "blue",
-                                            border: "1px solid black",
-                                            height: "20px"
-                                        }
-                                    }
-                                    >
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div 
-                                    style={{
-                                        float: "left"
-                                    }}
-
-                                    >Speed</div>
-                                    <div style={{    
-                                        float: "right"
-                                    }}>{this.stats[5]['base_stat']}</div>
-                                </td>
-                                <td className="statLabel">
-                                    <div className="statbar" style= {{
-                                            width: this.stats[5]['base_stat'] / 255 * 100,
-                                            backgroundColor: "yellow",
-                                            border: "1px solid black",
-                                            height: "20px"
-                                        }
-                                    }
-                                    >
-                                    </div>
-                                </td>
-                            </tr>
-                    
+                            {this.getStats()}
                         </tbody>
                     </table>
                 </div>
@@ -185,5 +49,37 @@ export default class Information extends React.Component {
 
     capitalize(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    getStats() {
+        this.stats = this.info.stats;
+        let rows = []
+        for (let stat of this.stats) {
+            rows.push(
+            <tr>
+                <td>
+                    <div 
+                    style={{
+                        float: "left"
+                    }}
+                    >{stat.stat.name}</div>
+                    <div style={{
+                        float: "right"
+                    }}>{stat['base_stat']}</div>
+                </td>
+                <td className="statLabel">
+                    <div className="statbar" style= {{
+                            width: stat['base_stat'] / 255 * 100,
+                            backgroundColor: this.colours[stat.stat.name],
+                            border: "1px solid black",
+                            height: "20px"
+                        }
+                    }
+                    >
+                    </div>
+                </td>
+            </tr>);
+        }
+        return rows;
     }
 }
