89c3b3d47c69772471621c9a106839b7b652e4c8
Office-Stapler <raymond.lam@outlook.com.au>
2020-12-15 16:10:56 +1100

    Added header

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchComponent from './components/search/Search.js';
+import Header from './components/header/header.js';
 import './App.css';
 
 export default function App() {
     return (
         <React.Fragment>
+            <Header />
             <SearchComponent />
         </React.Fragment>
     )
